1222363d59d61de1c45cfadf885aa766b9141598
alamosha <121043423+alamosha@users.noreply.github.com>
2023-01-29 02:34:22 +0200

    Update gitignore File

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -8,4 +8,4 @@ export AWS_BUCKET=arn:aws:s3:::zabuck012999
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export URL=http://udagram-api-dev.us-east-1.elasticbeanstalk.com
